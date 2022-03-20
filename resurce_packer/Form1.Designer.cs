ab9567ac02c2daa8d9f91af4ee80d14d710c236a
Sacamoto84 <87524340+Sacamoto84@users.noreply.github.com>
2022-03-20 18:53:04 +0300

    +1

--- resurce_packer/Form1.Designer.cs
+++ resurce_packer/Form1.Designer.cs
@@ -304,8 +304,6 @@ namespace resurce_packer
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(307, 477);
             this.dataGridView1.TabIndex = 32;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            this.dataGridView1.CurrentCellChanged += new System.EventHandler(this.dataGridView1_CurrentCellChanged);
             this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
             // 
             // Column1
